1a999455a71e51aba89bfcae25bf713e5a39883a
秋雨落 <i@rain.cx>
2025-10-23 18:45:49 +0800

    Revert no masked scroll viewport.
    Signed-off-by: 秋雨落 <i@rain.cx>

--- nbtedit-common/src/main/java/cx/rain/mc/nbtedit/gui/component/ScrollableViewport.java
+++ nbtedit-common/src/main/java/cx/rain/mc/nbtedit/gui/component/ScrollableViewport.java
@@ -5,7 +5,6 @@ import net.minecraft.client.gui.narration.NarrationElementOutput;
 import net.minecraft.client.input.KeyEvent;
 import net.minecraft.client.input.MouseButtonEvent;
 import net.minecraft.network.chat.Component;
-import net.minecraft.util.Mth;
 
 public class ScrollableViewport extends AbstractComposedComponent {
 
@@ -73,19 +72,20 @@ public class ScrollableViewport extends AbstractComposedComponent {
 
     @Override
     protected void renderWidget(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
+        var maskedMouseX = (int) getMaskedX(mouseX);
+        var maskedMouseY = (int) getMaskedY(mouseY);
+
         var maxX = getX() + getWidth() - (shouldShowVerticalBar() ? getScrollBarWidth() : 0);
         var maxY = getY() + getHeight() - (shouldShowHorizontalBar() ? getScrollBarWidth() : 0);
-        guiGraphics.enableScissor(getX(), getY(), maxX, maxY);
+
         guiGraphics.pose().pushMatrix();
+        guiGraphics.enableScissor(getX(), getY(), maxX, maxY);
+        guiGraphics.pose().translate(getX() - getScrollXOffset(), getY() - getScrollYOffset());
 
-        for (var c : getChildren()) {
-            c.setX(c.getX() - getScrollXOffset());
-            c.setY(c.getY() - getScrollYOffset());
-        }
-        super.renderWidget(guiGraphics, mouseX, mouseY, partialTick);
+        super.renderWidget(guiGraphics, maskedMouseX, maskedMouseY, partialTick);
 
-        guiGraphics.pose().popMatrix();
         guiGraphics.disableScissor();
+        guiGraphics.pose().popMatrix();
 
         if (shouldShowVerticalBar()) {
             verticalScrollBar.render(guiGraphics, mouseX, mouseY, partialTick);
@@ -124,6 +124,21 @@ public class ScrollableViewport extends AbstractComposedComponent {
         }
     }
 
+    private double getMaskedX(double rawX) {
+        return rawX - getX() + getScrollXOffset();
+    }
+
+    private double getMaskedY(double rawY) {
+        return rawY - getY() + getScrollYOffset();
+    }
+
+    private MouseButtonEvent getMaskedMouseEvent(MouseButtonEvent rawEvent) {
+        var mouseX = rawEvent.x();
+        var mouseY = rawEvent.y();
+        var button = rawEvent.buttonInfo();
+        return new MouseButtonEvent(getMaskedX(mouseX), getMaskedY(mouseY), button);
+    }
+
     @Override
     public boolean mouseClicked(MouseButtonEvent event, boolean isDoubleClick) {
         var mouseX = event.x();
@@ -137,7 +152,7 @@ public class ScrollableViewport extends AbstractComposedComponent {
             return horizontalScrollBar.mouseClicked(event, isDoubleClick);
         }
 
-        return super.mouseClicked(event, isDoubleClick);
+        return super.mouseClicked(getMaskedMouseEvent(event), isDoubleClick);
     }
 
     @Override
@@ -153,25 +168,20 @@ public class ScrollableViewport extends AbstractComposedComponent {
             return horizontalScrollBar.mouseReleased(event);
         }
 
-        return super.mouseReleased(event);
+        return super.mouseReleased(getMaskedMouseEvent(event));
     }
 
     @Override
-    public boolean mouseDragged(MouseButtonEvent event, double nowMouseX, double nowMouseY) {
-        var mouseX = event.x();
-        var mouseY = event.y();
-        var deltaX = mouseX - event.x();
-        var deltaY = mouseY - event.y();
-
-        if (shouldShowVerticalBar() && verticalScrollBar.isDragging() && deltaY != 0) {
-            return verticalScrollBar.mouseDragged(event, nowMouseX, nowMouseY);
+    public boolean mouseDragged(MouseButtonEvent event, double deltaX, double deltaY) {
+        if (shouldShowVerticalBar() && verticalScrollBar.isScrolling() && deltaY != 0) {
+            return verticalScrollBar.mouseDragged(event, deltaX, deltaY);
         }
 
-        if (shouldShowHorizontalBar() && horizontalScrollBar.isDragging() && deltaX != 0) {
-            return horizontalScrollBar.mouseDragged(event, nowMouseX, nowMouseY);
+        if (shouldShowHorizontalBar() && horizontalScrollBar.isScrolling() && deltaX != 0) {
+            return horizontalScrollBar.mouseDragged(event, deltaX, deltaY);
         }
 
-        return super.mouseDragged(event, nowMouseX, nowMouseY);
+        return super.mouseDragged(getMaskedMouseEvent(event), deltaX, deltaY);
     }
 
     @Override
